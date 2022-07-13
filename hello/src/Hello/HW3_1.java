ff7f74ec626a2e831ce4a8755dda14645099d1a1
125c <jaguarkinght@gmail.com>
2022-07-13 10:49:48 +0800

    HW3-1

--- hello/src/Hello/HW3_1.java
+++ hello/src/Hello/HW3_1.java
@@ -7,22 +7,107 @@ public class HW3_1 {
 	public static void main(String[] args) {
 		// TODO 自動產生的方法 Stub
 
-		int z=20,i,j=0,x;
-//		Scanner sc=new Scanner(System.in);	
-//		System.out.print("請輸入int:");
-//		z=sc.nextInt();
-//		sc.close();
+		int z,n;//s起始e結束i增加量
+		Scanner sc=new Scanner(System.in);	
+		System.out.print("請輸入1-9:");
+		z=sc.nextInt();
+		sc.close();
 
-		for(x=0;x<7;x+=3) {
-//			for(int q=1;q<z;q++) {
-				for(j=1;j<10;j++) {
-					for(i=1;i<4;i++) {
-						System.out.print((i+x)+"×"+j+"＝"+(i+x)*j+",\t\t");
-					}System.out.println();
-				}System.out.println("^ↀᴥↀ^");
-//			}
+		switch(z) {
+		case 1:
+			for(n=1;n<10;n++) {
+				System.out.println(n+"×1＝"+n);
+			}
+			System.out.println("^ↀᴥↀ^");
+			break;
+		case 2:
+			for(n=1;n<10;n++) {
+				System.out.println(n+"×1＝"+n+"\t\t"+n+"×2＝"+(n*2));
+			}
+			System.out.println("^ↀᴥↀ^");
+			break;
+		case 3:
+			for(n=1;n<10;n++) {
+				System.out.println(n+"×1＝"+n+"\t\t"+n+"×2＝"+(n*2)+"\t\t"+n+"×3＝"+(n*3));
+			}
+			System.out.println("^ↀᴥↀ^");
+			break;
+		case 4:
+			for(n=1;n<10;n++) {
+				System.out.println(n+"×1＝"+n+"\t\t"+n+"×2＝"+(n*2)+"\t\t"+n+"×3＝"+(n*3));
+			}
+			System.out.println("^ↀᴥↀ^");
+			for(n=1;n<10;n++) {
+				System.out.println(n+"×4＝"+(n*4));
+			}
+			System.out.println("^ↀᴥↀ^");
+			break;
+		case 5:
+			for(n=1;n<10;n++) {
+				System.out.println(n+"×1＝"+n+"\t\t"+n+"×2＝"+(n*2)+"\t\t"+n+"×3＝"+(n*3));
+			}
+			System.out.println("^ↀᴥↀ^");
+			for(n=1;n<10;n++) {
+				System.out.println(n+"×4＝"+(n*4)+"\t\t"+n+"×5＝"+(n*5));
+			}
+			System.out.println("^ↀᴥↀ^");
+			break;
+		case 6:
+			for(n=1;n<10;n++) {
+				System.out.println(n+"×1＝"+n+"\t\t"+n+"×2＝"+(n*2)+"\t\t"+n+"×3＝"+(n*3));
+			}
+			System.out.println("^ↀᴥↀ^");
+			for(n=1;n<10;n++) {
+				System.out.println(n+"×4＝"+(n*4)+"\t\t"+n+"×5＝"+(n*5)+"\t\t"+n+"×6＝"+(n*6));
+			}
+			System.out.println("^ↀᴥↀ^");
+			break;
+		case 7:
+			for(n=1;n<10;n++) {
+				System.out.println(n+"×1＝"+n+"\t\t"+n+"×2＝"+(n*2)+"\t\t"+n+"×3＝"+(n*3));
+			}
+			System.out.println("^ↀᴥↀ^");
+			for(n=1;n<10;n++) {
+				System.out.println(n+"×4＝"+(n*4)+"\t\t"+n+"×5＝"+(n*5)+"\t\t"+n+"×6＝"+(n*6));
+			}
+			System.out.println("^ↀᴥↀ^");
+			for(n=1;n<10;n++) {
+				System.out.println(n+"×7＝"+n);
+			}
+			System.out.println("^ↀᴥↀ^");
+			break;
+		case 8:
+			for(n=1;n<10;n++) {
+				System.out.println(n+"×1＝"+n+"\t\t"+n+"×2＝"+(n*2)+"\t\t"+n+"×3＝"+(n*3));
+			}
+			System.out.println("^ↀᴥↀ^");
+			for(n=1;n<10;n++) {
+				System.out.println(n+"×4＝"+(n*4)+"\t\t"+n+"×5＝"+(n*5)+"\t\t"+n+"×6＝"+(n*6));
+			}
+			System.out.println("^ↀᴥↀ^");
+			for(n=1;n<10;n++) {
+				System.out.println(n+"×7＝"+(n*7)+"\t\t"+n+"×8＝"+(n*8));
+			}
+			System.out.println("^ↀᴥↀ^");
+			break;
+		case 9:
+			for(n=1;n<10;n++) {
+				System.out.println(n+"×1＝"+n+"\t\t"+n+"×2＝"+(n*2)+"\t\t"+n+"×3＝"+(n*3));
+			}
+			System.out.println("^ↀᴥↀ^");
+			for(n=1;n<10;n++) {
+				System.out.println(n+"×4＝"+(n*4)+"\t\t"+n+"×5＝"+(n*5)+"\t\t"+n+"×6＝"+(n*6));
+			}
+			System.out.println("^ↀᴥↀ^");
+			for(n=1;n<10;n++) {
+				System.out.println(n+"×7＝"+(n*7)+"\t\t"+n+"×8＝"+(n*8)+"\t\t"+n+"×9＝"+(n*6));
+			}
+			System.out.println("^ↀᴥↀ^");
+			break;
+		default:
+			System.out.println("不要亂按！(／‵Д′)／~ ╧╧");
 		}
-				
+
 	}
 
 }
